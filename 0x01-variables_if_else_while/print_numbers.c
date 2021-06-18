e44410e480814a9f25df20c9c8763205413ca887
root <elishahezekiah903@gmail.com>
2021-06-18 10:48:40 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int any;
+
+	for (any = 0; any <= 10; a++)
+	{
+		printf(any);
+	}
+	printf('\n');
+	return (0);
+}
