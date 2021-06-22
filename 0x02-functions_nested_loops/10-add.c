bc236fa322cbabc7623fa3ea34e1fd98ae3bc3a7
root <elishahezekiah903@gmail.com>
2021-06-22 11:33:13 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - adds to numbers and returns the sum
+ * @a: a parameter
+ * @b: a parameter
+ * Return: gives the result of the two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
