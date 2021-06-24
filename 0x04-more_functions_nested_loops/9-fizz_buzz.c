5af8565bf328d868026750607b76bb28334df7b2
root <elishahezekiah903@gmail.com>
2021-06-24 13:58:09 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
+
 /**
- * main - main thing works here
- * Return: none
- */
+* main - prints Fizz for int divisible by 3 and Buzz for 5
+*
+* Return: int
+*/
+
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0)
+		else if (i % 3 != 0 && i % 5 == 0)
 		{
 			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
 		}
 		else
 		{
