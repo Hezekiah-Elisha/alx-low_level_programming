10a24a1cee3e759c00d2f211b5dea7cbdb673f0b
root <elishahezekiah903@gmail.com>
2021-06-24 13:47:22 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,23 +8,23 @@ void fizz_buzz(void)
 {
 	int i, j;
 
-        for (i = 1; i <= 100; i++)
-        {
-            if (i % 3 == 0 && i % 5 == 0)
-            {
-                printf("fizzbuzz");
-            }
-            else if (i % 3 == 0)
-            {
-                printf("fizz");
-            }
-            else if (i % 5 == 0)
-            {
-                printf("buzz");
-            }
-            else
-            {
-                printf("%d", i);
-            }
-        }
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
 }
