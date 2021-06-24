b2c2f60e8d57e5450337847822e7acd1d8a5690c
root <elishahezekiah903@gmail.com>
2021-06-24 13:41:44 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * fizz_buzz - main thing works here
+ * Return: none
+ */
+void fizz_buzz(void)
+{
+	int i, j;
+
+        for (i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                printf("fizzbuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                printf("fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                printf("buzz");
+            }
+            else
+            {
+                printf("%d", i);
+            }
+        }
+}
