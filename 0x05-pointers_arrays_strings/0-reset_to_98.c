8b642a0e67d4208f67a0c9af2f397b3c8498092d
root <elishahezekiah903@gmail.com>
2021-06-28 09:43:45 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * reset_to_98 - resets *n to 98
- * @*n: this is a parameter to the function
+ * @n: this is a parameter to the function
  * Return: return an nothing
  */
 void reset_to_98(int *n)
