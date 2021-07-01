14c73164c9a5d5aa97feb5a2f2d5e7f5d0ce4d1e
root <elishahezekiah903@gmail.com>
2021-07-01 06:53:37 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * length - finds the length of an array
+ * @i: first perimeter
+ * Return: an int
+ */
+int length(char *i)
+{
+	int s = 0;
+
+	while (i[s] != '\0')
+	{
+		s++;
+	}
+	return (s);
+}
+/**
+ * _strcat - concertinates two strings
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: third parameter
+ * Return: returns a char
+ */
+char *_strcat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = length(dest);
+	for (j = 0; src[j] != '\0' && j < n; ++j)
+	{
+		dest[i] = src[j];
+		++i;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
