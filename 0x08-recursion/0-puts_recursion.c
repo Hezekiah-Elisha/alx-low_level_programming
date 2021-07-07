56a01689e891c1124ca59e29abd5149472130ae9
root <elishahezekiah903@gmail.com>
2021-07-07 09:51:10 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line
+ * @s: first parameter
+ * Return: none
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		break;
+	}
+	_putchar('\n');
+	_put_recursion(s + 1);
+}
