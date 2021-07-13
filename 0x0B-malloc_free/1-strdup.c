9d276e7cd4fbea4ce03f98d2c0d8862110f1b555
root <elishahezekiah903@gmail.com>
2021-07-13 13:28:39 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,36 +1,29 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * _strdup - a function that returns a pointer
- * @str: param 1
- * Return: a char
+ * _strdup - duplicate the string str and return a pointer
+ * @str: string to duplicate
+ * Return: pointer to copy of string
  */
 char *_strdup(char *str)
 {
-	int a, b;
-	char *j;
+	int i = 0;
+	char *p;
 
-	j = malloc(1 + (sizeof(*str) * b));
 	if (str == NULL)
-	{
 		return (NULL);
-	}
-	else
+	while (str[i])
+		i++;
+	p = malloc(sizeof(char) * i + 1);
+	i = 0;
+	if (p == NULL)
+		return (NULL);
+	while (str[i])
 	{
-		for (b = 0; str[b] != 0; b++)
-		{}
-		if (j == NULL)
-		{
-			return (NULL);
-		}
-		else
-		{
-			for (a = 0; a < b; a++)
-			{
-				j[a] = str[a];
-			}
-		}
-		return (j);
+		p[i] = str[i];
+		i++;
 	}
+	p[i] = '\0';
+	return (p);
 }
