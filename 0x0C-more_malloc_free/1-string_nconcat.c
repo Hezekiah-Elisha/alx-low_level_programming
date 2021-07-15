e327e5ac4f90741f1f8283e96710543ba8c877ea
root <elishahezekiah903@gmail.com>
2021-07-15 12:56:28 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,20 @@
 #include "holberton.h"
 #include <stdlib.h>
+
+/**
+ * _strlen - functions that calculate the length of str
+ * @str: string
+ * Return: length of string
+ */
+unsigned int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len])
+		len++;
+	return (len);
+}
+
 /**
  * string_nconcat - functions that concatenate s1 with 'n' chars of s2
  * @s1: string1
@@ -17,15 +32,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 	/* calculate total_len of n > or < len_s2 */
-	if (n <= strlen(s2))
-		total_len = strlen(s1) + n;
+	if (n <= _strlen(s2))
+		total_len = _strlen(s1) + n;
 	else
-		total_len = strlen(s1) + strlen(s2);
+		total_len = _strlen(s1) + _strlen(s2);
 	/*malloc for p, si es NULL -> p=NULL || len = 0 => ""*/
 	p = malloc(sizeof(char) * (total_len + 1));
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < strlen(s1); i++)
+	for (i = 0; i < _strlen(s1); i++)
 		*(p + i) = *(s1 + i);
 	for (; i < total_len; i++)
 		*(p + i) = *(s2 + j++);
