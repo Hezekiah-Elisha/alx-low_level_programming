e96aa27e24ce74e34eb2628dc9a4531227bc6ef7
root <elishahezekiah903@gmail.com>
2021-07-21 16:14:10 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,23 @@
+#include "3-calc.h"
+/**
+ * main - test with argv
+ * @argc: number of argv
+ * @argv: arguments input of main
+ * Return: Return of operation
+ */
+int main(int argc, char **argv)
+{
+	int a, b, result;
+
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+		printf("Error\n"), exit(100);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	result = get_op_func(argv[2])(a, b);
+	printf("%i\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - a funtion
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: an int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - a funtion
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: an int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - a funtion
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: an int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - a funtion
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: an int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - a funtion
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: an int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
