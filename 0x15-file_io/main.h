cc4a73d42bcc9387dbdf6254b8d90b98a1f79c88
root <elishahezekiah903@gmail.com>
2021-08-09 11:18:40 -0700

    Tread lightly, she is near

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,4 +1,11 @@
 #ifndef FILE_H
 #define FILE_H
 
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcnt1.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
 #endif
