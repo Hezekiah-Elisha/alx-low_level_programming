18ad51a3bbe25b346f5d411a1e91e703398f8df6
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-03-30 20:15:52 +0300

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,22 @@
 #include "search_algos.h"
 
 /**
+ * print_scope - Print the scope of the array to search
+ * @array: pointer to array
+ * @left: left side to start the search
+ * @right: last value to check in the array
+ * Return: void
+ */
+void print_scope(int *array, int left, int right)
+{
+	int i;
+
+	for (i = left; i < right; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+}
+
+/**
  * binary_search - searches for a value in an array of integers
  * @array: a pointer to the first element of the array to search in
  * @size: number of elements in array
